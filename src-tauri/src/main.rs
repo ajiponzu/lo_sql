5196c3850b71dd4fd17d07eaae83439f35ee267c
mita <cpjvmm@outlook.jp>
2022-09-21 19:28:23 +0900

    add: データベース接続とデータ取得処理を追加

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,9 +3,41 @@
     windows_subsystem = "windows"
 )]
 
+#[derive(sqlx::FromRow, Debug)]
+struct TableInf {
+    pub name: String,
+}
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+async fn greet(name: &str) -> Result<String, ()> {
+    let user = "root";
+    let password = "password";
+    let host = "127.0.0.1";
+    let port = "3306";
+
+    let database_url = format!("mysql://{}:{}@{}:{}/{}", user, password, host, port, &name);
+
+    let pool_ret = sqlx::mysql::MySqlPoolOptions::new()
+        .max_connections(20)
+        .connect(&database_url)
+        .await;
+    let message_wrap: Result<String, ()> = match pool_ret {
+        Ok(pool) => {
+            println!("db_connected....");
+            let table_infs = sqlx::query_as::<_, TableInf>(
+                "select table_name as name from information_schema.tables where table_schema = ?",
+            )
+            .bind::<String>(name.to_string())
+            .fetch_all(&pool)
+            .await;
+            match table_infs {
+                Ok(inf) => Result::Ok(format!("tables: {:?}", inf)),
+                Err(e) => Result::Ok(format!("sql_execute_error: {}", e.to_string())),
+            }
+        }
+        Err(e) => Result::Ok(format!("sql_execute_error: {}", e.to_string())),
+    };
+    message_wrap
 }
 
 fn main() {
