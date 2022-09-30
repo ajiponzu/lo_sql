555f4a60a96fd66dc8d990b4d9da452c9114e53a
mita <cpjvmm@outlook.jp>
2022-10-01 00:08:52 +0900

    add: ボタンを４つ追加

--- src/Visual.tsx
+++ src/Visual.tsx
@@ -2,6 +2,35 @@ import { useParams } from "react-router-dom";
 import { invoke } from "@tauri-apps/api/tauri";
 import { useEffect, useState } from "react";
 
+const Header = () => {
+  return (
+    <div className="VisualHeader">
+      <div className="buttons">
+        <button>
+          🗝️
+          <br />
+          Login
+        </button>
+        <button>
+          📖
+          <br />
+          Data
+        </button>
+        <button>
+          📑
+          <br />
+          Details
+        </button>
+        <button>
+          ↺
+          <br />
+          Reload
+        </button>
+      </div>
+    </div>
+  );
+};
+
 const convertNameList = (json: Array<any>) => {
   let retArray = [] as Array<string>;
   json.forEach((elem) => {
@@ -29,7 +58,12 @@ const Visual = () => {
 
   console.log(tableNames);
 
-  return <div className="Visual">Visual</div>;
+  return (
+    <div className="Visual">
+      <Header />
+      Visual
+    </div>
+  );
 };
 
 export default Visual;
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./Login";
 import "./style.css";
+import "./Visual.css";
 import Visual from "./Visual";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
